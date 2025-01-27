b6c07b6d20c75847e621ebe2d89a0db6cfa19466
Walter Pereira Cruz <wpereira04@vt.edu>
2025-01-27 18:58:53 -0500

    changed the code

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -9,6 +9,76 @@
 #include "matrix.h"
 
 
+
+// creates a zero filled matrix of dimensions of rows and columns
+Matrix createMatrix(int rows, int cols) {
+	Matrix matri(rows, std::vector<double>(cols, 0.0));
+	return matri;
+}
+
+// adds two matrices and returns matrix. throws exception on invalid
+Matrix addMatrices(const Matrix& m1, const Matrix& m2) {
+	// throws an exception if the matrix operation is not possible
+	if (m1.size() != m2.size()||m1[0].size() != m2[0].size()) {
+		throw std::invalid_argument("Matrices are of different sizes");
+	}
+
+	int rows = m1.size(); // rows of m1 and m2 are same
+	int cols = m1[0].size(); // cols of m1 and m2 are same
+	Matrix res = createMatrix(rows, cols); // result matrix
+	// goes through all elements in matrices
+	for (std::size_t i = 0; i < rows; i++) { // loops through rows
+		for (std::size_t j = 0; j < cols; ++j) { // loops through cols
+			res[i][j] = m1[i][j] + m2[i][j]; // adds the elements
+		}
+	}
+	return res;
+}
+
+// Function to subtract two matrices
+Matrix subtractMatrices(const Matrix& m1, const Matrix& m2) {
+	// throws an exception if the matrix operation is not possible
+	if (m1.size() != m2.size() || m1[0].size() != m2[0].size()) {
+		throw std::invalid_argument("Matrices are of different sizes");
+	}
+
+	int rows = m1.size(); // rows of m1 and m2 are same
+	int cols = m1[0].size(); // cols of m1 and m2 are same
+	Matrix res = createMatrix(rows, cols); // result matrix
+
+	for (std::size_t i = 0; i < rows; i++) { // loops through rows
+		for (std::size_t j = 0; j < cols; j++) { // loops through cols
+			res[i][j] = m1[i][j] - m2[i][j]; // subtracts the elements
+		}
+	}
+	return res;
+}
+
+// Function to multiply two matrices
+Matrix multiplyMatrices(const Matrix& m1, const Matrix& m2) {
+	// throws an exception if the matrix operation is not possible
+	if (m1[0].size() != m2.size()) {
+		throw std::invalid_argument("Matrices are of incompatible sizes");
+	}
+
+	int rows = m1.size(); // rows of m1
+	int cols = m2[0].size(); // cols of m2
+	int innerDim = m2.size(); // rows of m2 must be the same as cols of m1
+	Matrix res = createMatrix(rows, cols); // result matrix
+
+	for (std::size_t i = 0; i < rows; i++) {
+		for (std::size_t j = 0; j < cols; j++) {
+			double sum = 0;
+			for (std::size_t k = 0; k < innerDim; k++) { // gets sum for each element in the result matrix
+				sum += m1[i][k] * m2[k][j];
+			}
+			res[i][j] = sum; // puts sum into res matrix
+		}
+	}
+	return res;
+}
+
+/*
 Matrix createMatrix(int rows, int cols) {
 	// resizes the vectors to the proper sizes
 	std::vector<std::vector<double>> curr;
@@ -67,4 +137,34 @@ Matrix multiplyMatrices(const Matrix& m1, const Matrix& m2) {
 		}
 	}
 	return resu;
+	if ((m1.size() != m2.size()) || (m1[0].size() != m2[0].size())) {
+		throw std::invalid_argument("Matrices are of incompatible sizes");
+	}
+	Matrix res = createMatrix(m1[0].size(), m1.size());
+	// subtracts the different elements of the matrices one by one
+	for (std::size_t i = 0; i < (m1.size() * m1[0].size()); i++) {
+		int row = floor(i / m1.size()); // basic calculation for the row using i and the dimensions of the matrices
+		int col = i % m1.size(); // basic calculation for the column using i and the dimensions of the matrices
+		res[col][row] = m1[col][row] * m2[col][row]; // calculates difference and puts in corresponding result space
+	}
+	return res;
+	// throws an exception if the matrix operation is not possible
+	// TODO: Try doing literal matrix multiplication even if it isn't an actual operation
+	// columns of A must be same size of Rows of B
+	if (m1.size() != m2[0].size()) {
+		throw std::invalid_argument("Matrices are of incompatible sizes");
+	}
+	// creates result matrix
+	Matrix res = createMatrix(m1[0].size(), m2.size());
+	std::size_t sizeMult = m1.size();
+	if (sizeMult < m2.size()) sizeMult = m2.size();
+	for (std::size_t i = 0; i < m2.size() * (m1.size() * m1[0].size()); i++) {
+		int row1 = floor(i / (2 * sizeMult)); // row1 is used by m1 due to the nature of dot product. 3x2 * 2x3  TODO: THIS LINE JUST SEEMS WRONG
+		int row2 = floor(i / m1.size()); //sets row2 so it the column of m2 is goes 0 then 1
+		row2 = row2 % m2.size(); // makes it so when m1 goes to row 2, the column "pointer" is reset to 0
+		int col = i % m1.size(); // this variable acts as columns for m1 but rows for m2
+		res[row2][row1] += m1[col][row1] * m2[row2][col]; // name of variables don't make sense for m2 but the same values can be used as shown
+	}
+	return res;
 }
+*/
--- test/test.cpp
+++ test/test.cpp
@@ -9,6 +9,101 @@
 #include "catch2/catch.hpp"
 #include "matrix.h"
 
+TEST_CASE("Testing matrix.cpp definitions") {
+	// Matrices to be used in tests
+	Matrix s = createMatrix(2, 3);
+	s[0][0] = 1;
+	s[0][1] = 2;
+	s[0][2] = 3;
+	s[1][0] = 4;
+	s[1][1] = 5; 
+	s[1][2] = 6;
+
+	Matrix t = createMatrix(3, 2);
+	t[0][0] = 6;
+	t[0][1] = 5;
+	t[1][0] = 4;
+	t[1][1] = 3;
+	t[2][0] = 2;
+	t[2][1] = 1;
+
+	Matrix same = createMatrix(2, 2);
+	same[0][0] = 1;
+	same[0][1] = 3;
+	same[1][0] = 2;
+	same[1][1] = 4;
+
+	Matrix diff = createMatrix(2, 2);
+	diff[0][0] = 4; 
+	diff[0][1] = 2;
+	diff[1][0] = 3;
+	diff[1][1] = 1;
+
+	SECTION("Testing createMatrix") {
+		Matrix test = createMatrix(3, 2);
+		REQUIRE(test.size() == 3);
+		for (int i = 0; i < test.size(); i++) {
+			REQUIRE(test[i].size() == 2);
+		}
+	}
+
+	SECTION("Testing addMatrices") {
+		Matrix expect = same;  // copy of `same`
+		expect[0][0] = 2;
+		expect[0][1] = 6;  
+		expect[1][0] = 4;
+		expect[1][1] = 8;
+
+		REQUIRE(addMatrices(same, same) == expect);  // tests adding same values
+
+		expect[0][0] = 5;
+		expect[0][1] = 5;
+		expect[1][0] = 5;
+		expect[1][1] = 5;
+
+		REQUIRE(addMatrices(same, diff) == expect);  // tests adding different values
+		REQUIRE_THROWS(addMatrices(s, t));  // tests throw
+	}
+
+	SECTION("Testing subtractMatrices") {
+		Matrix expct = createMatrix(2, 2);  // Zero filled matrix
+
+		REQUIRE(subtractMatrices(same, same) == expct);  // tests subtracting same values
+
+		expct[0][0] = -3;
+		expct[0][1] = 1; 
+		expct[1][0] = -1;
+		expct[1][1] = 3;
+
+		REQUIRE(subtractMatrices(same, diff) == expct);  // tests subtracting different values
+		REQUIRE_THROWS(subtractMatrices(s, t));  // tests throw
+	}
+
+	SECTION("Testing multiplyMatrices") {
+		Matrix expct1 = createMatrix(2, 2);
+		expct1[0][0] = 20;
+		expct1[0][1] = 14;  
+		expct1[1][0] = 56; 
+		expct1[1][1] = 41;
+
+		Matrix expct2 = createMatrix(3, 3);  
+		expct2[0][0] = 26;
+		expct2[0][1] = 37;
+		expct2[0][2] = 48;
+		expct2[1][0] = 16;
+		expct2[1][1] = 23;
+		expct2[1][2] = 30;
+		expct2[2][0] = 6; 
+		expct2[2][1] = 9; 
+		expct2[2][2] = 12;
+
+		REQUIRE(multiplyMatrices(s, t) == expct1);  // tests matrix multiplication in one way
+		REQUIRE(multiplyMatrices(t, s) == expct2);  // tests matrix multiplication in the reverse way
+		REQUIRE_THROWS(multiplyMatrices(s, diff));  // tests throw
+	}
+}
+
+/*
 TEST_CASE("Testing matrix.cpp") {
 	// matrices to be used in tests. Makes it easier to write tests.
 	Matrix s = createMatrix(2, 3);
@@ -88,3 +183,4 @@ TEST_CASE("Testing matrix.cpp") {
 		REQUIRE_THROWS(multiplyMatrices(s, diff)); // tests throw
 	}
 }
+*/

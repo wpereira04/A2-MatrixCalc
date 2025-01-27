578397862772b4a9f56fa5bb79f9a51d2ff79844
Walter Pereira Cruz <wpereira04@vt.edu>
2025-01-27 17:43:41 -0500

    once again changed how matrix multiplication works

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -53,19 +53,18 @@ Matrix subtractMatrices(const Matrix& m1, const Matrix& m2) {
 }
 
 Matrix multiplyMatrices(const Matrix& m1, const Matrix& m2) {
-	if ((m1.size() != m2.size())||(m1[0].size()!= m2[0].size())) {
+	if ((m1.size() != m2[0].size()) || (m1.size() == 0)) {
 		throw std::invalid_argument("Matrices are of incompatible sizes");
 	}
-	Matrix res = createMatrix(m1[0].size(), m1.size());
-	// subtracts the different elements of the matrices one by one
-	for (std::size_t i = 0; i < (m1.size() * m1[0].size()); i++) {
-		int row = floor(i / m1.size()); // basic calculation for the row using i and the dimensions of the matrices
-		int col = i % m1.size(); // basic calculation for the column using i and the dimensions of the matrices
-		res[col][row] = m1[col][row] * m2[col][row]; // calculates difference and puts in corresponding result space
+	Matrix resu = createMatrix(m1[0].size(),m2.size());
+	for (std::size_t i = 0; i < m1[0].size(); i++) {
+		for (std::size_t j = 0; j < m2.size(); j++) {
+			double sum = 0;
+			for (std::size_t k = 0; k < m1.size(); k++) {
+				sum += m1[k][i] * m2[j][k];
+			}
+			resu[j][i] = sum;
+		}
 	}
-	return res;
-	// throws an exception if the matrix operation is not possible
-	// TODO: Try doing literal matrix multiplication even if it isn't an actual operation
-	// columns of A must be same size of Rows of B
-	
+	return resu;
 }
